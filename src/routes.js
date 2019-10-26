57ba0a062b4e8f5dafef79e73230cec3d59694f7
GabrielFraga <gabrielgoog@gmail.com>
2019-10-25 22:01:22 -0300

    autenticar token do admin

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,16 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentsController';
 
+import authMiddleware from './app/middleweres/auth';
+
 const routes = new Router();
 
 routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
 routes.get('/students', StudentController.getAll);
 routes.get('/students/:id', StudentController.getOne);
+routes.post('/students', StudentController.store);
 routes.put('/students/:id', StudentController.update);
 routes.delete('/students/:id', StudentController.delete);
 export default routes;
